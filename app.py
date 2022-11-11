7d1fab86b21c0856e27eec668296bacb9e79b662
Aniket Maurya <theaniketmaurya@gmail.com>
2022-11-11 12:34:36 +0530

    remove mps

--- app.py
+++ app.py
@@ -43,8 +43,6 @@ class ModelDemo(ServeGradio):
         model = WaveformerModel(**params["model_params"])
         if torch.cuda.is_available():
             device = torch.device("cuda")
-        elif torch.backends.mps.is_available():
-            device = torch.device("mps")
         else:
             device = torch.device("cpu")
         print(f"loading model on {device}")
